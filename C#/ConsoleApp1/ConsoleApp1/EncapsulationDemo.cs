643112ff939b30fba8af5bf2219acb9f66c02618
Bharath <bharath35@gmail.com>
2020-08-20 21:48:26 +0530

    Source Code

---
+++ C#/ConsoleApp1/ConsoleApp1/EncapsulationDemo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class EncapsulationDemo
+    {
+        private int x;
+        public int getX()
+        {
+            return x;
+        }
+        public void setX(int x)
+        {
+            if (x > 0)
+            {
+                this.x = x;
+            }
+            else
+            {
+                Console.WriteLine("Please Pass a positive value");
+            }
+        }
+    }
+    class Sample
+    {
+        //public static void Main()
+        //{
+        //    EncapsulationDemo e = new EncapsulationDemo();            
+        //    e.setX(10);
+        //    Console.WriteLine(e.getX());
+        //    e.setX(-50);
+        //    Console.WriteLine(e.getX());
+        //    Console.WriteLine("Press any key to exist");
+        //    Console.ReadKey();
+        //}
+    }
+}
---
+++ C#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            try
+            {                
+                //Code
+                int i = 0;
+                int j = 10;
+                Console.WriteLine(j / i);
+                Console.ReadLine();               
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine("Error" + ex.Message + " Stack Trace");
+                Console.ReadLine();
+            }
+            catch (Exception objex)
+            {
+                //Code                
+                Console.WriteLine("Error" + objex.Message  + " Stack Trace");
+                Console.ReadLine();
+            }
+            finally
+            {                
+                //code
+            }
+
+        }
+    }
+}
