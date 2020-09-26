4af3c8d7983e44123e52e268094360b03717a962
Bharath <bharath35@gmail.com>
2020-09-26 09:54:54 +0530

    added new file

---
+++ MVC/MVCApplication/MVCApplication/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCApplication.Models
+{
+    public class Department
+    {
+        public enum Gender
+        {
+            Male,
+            Female
+        }
+    }
+}
