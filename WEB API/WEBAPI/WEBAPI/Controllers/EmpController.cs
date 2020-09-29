1742dd6507f20f65fb19923a9d7352f0d9683664
Bharath <bharath35@gmail.com>
2020-09-29 21:23:23 +0530

    WEB API

---
+++ WEB API/WEBAPI/WEBAPI/Controllers/EmpController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WEBAPI.Controllers
+{
+    public class EmpController : ApiController
+    {
+        //GET:api/Emp/1
+        [HttpGet]
+        public string GetEmpName(int id)
+        {
+            string name = "";
+            if (id == 1)
+            {
+                name = "Test123";
+            }
+            else
+            {
+                name = "Test321";
+            }
+
+            return name;
+        }
+
+        //POST
+        //PUT
+        //DELETE
+    }
+}
